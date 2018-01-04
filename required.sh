0ef89bbe699ef2036c1b44b20619ec6f87334fa0
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-01-04 22:32:49 +0100

    Update required.sh

--- required.sh
+++ required.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install --save request
-npm install --save express
-npm install --save http
-npm install --save ws
-npm install --save 
+#npm install --save express
+#npm install --save http
+#npm install --save ws
+npm install --save fastify
