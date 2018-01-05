d6498a3297f2219a31da137c08e820fda00ba214
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-01-05 03:36:11 +0100

    Update ETH.php

--- backend/ETH.php
+++ backend/ETH.php
@@ -1,20 +1,20 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 require('lib.php');
-$btc = cryptobot("bitcoin");
-$dec_btc = json_decode($btc, true);
+$eth = cryptobot("ethereum");
+$dec_eth = json_decode($eth, true);
 $json_array = array(
         // =====================================================
         // /////////////////////////////////////////////////////
         // =====================================================
             // =====================================================
             // =====================================================
-            "RANK" => "{$dec_btc['0']['rank']}",
-            "NAME" => "{$dec_btc['0']['name']}",
-            "SYMBOL" => "{$dec_btc['0']['symbol']}",
-            "PRICE_USD" => "{$dec_btc['0']['price_usd']}",
-            "PRICE_EUR" => "{$dec_btc['0']['price_eur']}",
-            "CH24" => "{$dec_btc['0']['percent_change_24h']}"
+            "RANK" => "{$dec_eth['0']['rank']}",
+            "NAME" => "{$dec_eth['0']['name']}",
+            "SYMBOL" => "{$dec_eth['0']['symbol']}",
+            "PRICE_USD" => "{$dec_eth['0']['price_usd']}",
+            "PRICE_EUR" => "{$dec_eth['0']['price_eur']}",
+            "CH24" => "{$dec_eth['0']['percent_change_24h']}"
             // =====================================================
             // =====================================================
         // =====================================================
