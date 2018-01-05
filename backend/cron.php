adb80daa15451ea47485e955afef6c24d174c59a
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-01-05 01:33:46 +0100

    Update cron.php

--- backend/cron.php
+++ backend/cron.php
@@ -1,16 +1,7 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
+require('lib.php');
 
-function cryptobot($sym)
-{
-    $url="https://api.coinmarketcap.com/v1/ticker/$sym/?convert=EUR";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
 $btc = cryptobot("bitcoin");
 
 
